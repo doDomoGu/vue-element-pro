f8c9b1edc2dafd6ce2fb5081a257449a777cce0b
doDomoGu <kaka_71936410@qq.com>
2019-01-10 17:32:36 +0800

    基础路由 && 基础layout

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import BasicLayout from '@/views/layouts/BasicLayout'
+
+Vue.use(Router)
+
+//通用路由
+export const constantRouterMap = [
+  // {
+  //   path: '/login',
+  //   component: () => import('@/views/login/index'),
+  //   hidden: true,
+  // },
+  {
+    path: '/404',
+    component: () => import(/* webpackChunkName: "page404" */ '@/views/exception/404'),
+    hidden: true,
+  },
+  {
+    path: '',
+    redirect: 'home',
+    component: BasicLayout,
+    children: [
+      {
+        path: 'home',
+        component: () => import(/* webpackChunkName: "home" */ '@/views/Home'),
+        name: 'Home',
+        meta: { title: 'home', icon: 'dashboard', noCache: true },
+      },
+    ],
+  },
+  { path: '*', redirect: '/404', hidden: true },
+]
+
+export default new Router({
+  mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap,
+})
