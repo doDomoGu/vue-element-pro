0b3d69bfd827a3fa223e0b61cc1b19643100ee3c
doDomoGu <kaka_71936410@qq.com>
2019-01-12 23:06:18 +0800

    BasicLayout

--- src/views/layouts/BasicLayout.vue
+++ src/views/layouts/BasicLayout.vue
@@ -1,3 +1,39 @@
 <template>
-  <router-view/>
+  <el-container>
+    <el-aside :width="'200px'">
+      <sidebar/>  
+    </el-aside>
+    <el-container>
+      <el-header>
+        This is Header
+      </el-header>
+      <router-view/>
+      <el-footer>
+        This is Footer
+      </el-footer>
+    </el-container>
+  </el-container>
 </template>
+<script>
+// import { mapGetters } from 'vuex'
+import Sidebar from './sidebar/Index'
+
+export default {
+  components: { Sidebar },
+}
+</script>
+
+<style scoped>
+.el-container {
+  height: 100%;
+}
+.el-header {
+  text-align: center;
+  line-height: 60px;
+}
+.el-footer {
+  text-align: center;
+  line-height: 60px;
+}
+
+</style>
