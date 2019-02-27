a15b78c1dbb9b4bd711cb8a966e0bf127ab23ba0
doDomoGu <kaka_71936410@qq.com>
2019-02-27 15:24:33 +0800

    Mock Token检查 方法

--- src/mock/modules/user.js
+++ src/mock/modules/user.js
@@ -1,32 +1,22 @@
-import { return_format, getToken } from '../utils'
+import { return_format, checkToken } from '../utils'
 import users from '../models/user'
 
 export default {
   list: config => {
-    const token = getToken(config.url)
-
-    let flag = false
     let code = 0
     let data = null
     let msg = null
 
-    if(token){
-      for(let i in users){
-        if(users[i].token == token){
-          flag = true
-        }
-      }
-    }
-    
-    if(!flag) {
-      code = 1001
-      msg = 'Token 错误'
-    }else{
+    if(checkToken(config.url)){
       data = {
         list: users,
         total: users.length
       }
+    }else{
+      code = 1001
+      msg = 'Token 错误'
     }
+      
     return return_format(code, data, msg)
   },
   
--- src/mock/utils.js
+++ src/mock/utils.js
@@ -2,7 +2,7 @@ import url from 'url'
 import qs from 'qs'
 
 import { AUTH_TOKEN_KEY } from '@/config/constantVariables'
-
+import users from './models/user'
 
 export function return_format(code, data, msg){
   return {
@@ -16,4 +16,21 @@ export function getToken(_url) {
   const urlObj = url.parse(_url)
   const urlQuery = qs.parse(urlObj.query)
   return urlQuery[AUTH_TOKEN_KEY]
+}
+
+export function checkToken(_url) {
+  const urlObj = url.parse(_url)
+  const urlQuery = qs.parse(urlObj.query)
+  const token = urlQuery[AUTH_TOKEN_KEY]
+
+  let flag = false
+  if(token){
+    for(let i in users){
+      if(users[i].token == token){
+        flag = true
+      }
+    }
+  }
+  
+  return flag
 }
