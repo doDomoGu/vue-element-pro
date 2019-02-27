bae36d240f1b781e73756a7daa4187e2b99ba772
doDomoGu <kaka_71936410@qq.com>
2019-02-27 15:28:19 +0800

    Mock Token检查 方法

--- src/mock/modules/auth.js
+++ src/mock/modules/auth.js
@@ -1,5 +1,5 @@
 import qs from 'qs'
-import { return_format, getToken } from '../utils'
+import { return_format, getToken, checkToken } from '../utils'
 
 import users from '../models/user'
 
@@ -49,49 +49,33 @@ export default {
     return return_format(code, data, msg)
   },
   logout: config => {
-    const token = getToken(config.url)
-
-    let flag = false
     let code = 0
     let data = null
     let msg = null
 
-    if(token){
-      for(let i in users){
-        if(users[i].token == token){
-          flag = true
-          data = users[i]
-        }
-      }
-    }
-    
-    if(!flag) {
+    if(!checkToken(config.url)){
       code = 1001
       msg = 'Token 错误'
     }
     return return_format(code, data, msg)
   },
   info: config => {
-    const token = getToken(config.url)
-
-    let flag = false
     let code = 0
     let data = null
     let msg = null
 
-    if(token){
+    if(checkToken(config.url)){
+      const token = getToken(config.url)
       for(let i in users){
         if(users[i].token == token){
-          flag = true
           data = users[i]
         }
       }
-    }
-    
-    if(!flag) {
+    }else{
       code = 1001
       msg = 'Token 错误'
     }
+    
     return return_format(code, data, msg)
   },
 }
