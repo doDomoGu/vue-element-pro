d47c2a14633ed963e3a327bec5dea3d6c0b5e3d8
doDomoGu <kaka_71936410@qq.com>
2019-04-04 17:38:03 +0800

    storybook 增加参数调试插件 addon-knobs

--- stories/lazytree/default.js
+++ stories/lazytree/default.js
@@ -2,18 +2,30 @@ import { storiesOf } from '@storybook/vue';
 import LazyTree from '@/components/lazy-tree';
 import * as TreenodeApi from '@/api/treenode'
 
+
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
+
+
 storiesOf('Lazytree', module)
+  .addDecorator(withKnobs)
   .add('Default', () => ({
     components: { LazyTree },
     data() {
       return {
+        page_size: 13,
         defaultExpandedKeys: ['root']
       };
+    },
+    props: {
+      page_size : {
+        default: number('每页显示数', 8)
+      }
     },
     render(h) {
       return <lazy-tree 
         node-data={this.nodeData}
         default-expanded-keys={this.defaultExpandedKeys}
+        page-size={this.page_size}
       />;
     },
     methods: {
@@ -27,7 +39,7 @@ storiesOf('Lazytree', module)
             }
             params.page = node.data.currentPage
           }
-          params.page_size = 10
+          params.page_size = this.page_size //10
           params.data_type = 'middle'
           TreenodeApi.get(params).then( res => {
             if ( res.data && res.data.code === 0){
